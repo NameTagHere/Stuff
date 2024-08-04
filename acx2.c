8a3ee4aec5de433a4b2f4fbac5df089d53adffaf
NameTagHere <149911912+NameTagHere@users.noreply.github.com>
2024-08-04 01:10:23 -0700

    Create acx2.c

---
+++ acx2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+char anticipation;
+char expectation;
+char ab;
+int a;
+int b;
+//formula start
+anticipation = a;
+expectation = b;
+ab += a +=  b ;
+char intuition;
+char instinct;
+char cd;
+int c;
+int d;
+intuition = c;
+instinct = d;
+cd += c += d;
+c += b = d;
+c += a = b;
+/*//formula end, next line result expression of ab + cd.*/
+printf("%c",ab += cd);
+
+return 00;}
